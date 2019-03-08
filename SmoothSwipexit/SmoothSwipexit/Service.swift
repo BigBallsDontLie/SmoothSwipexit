3585d169fd6ab617c8a0b656a845380c34c0c92e
Johah <johah@axis.com>
2019-03-08 14:08:23 +0100

    Initial framework

---
+++ SmoothSwipexit/SmoothSwipexit/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  SmoothSwipexit
+//
+//  Created by Johan Helmertz on 2019-03-08.
+//  Copyright © 2019 Johan Helmertz. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+  private init() {}
+
+  static func doSomething() -> String {
+    return "Did something. Here you go homeboiii!"
+  }
+}
---
+++ SmoothSwipexit/SmoothSwipexit/SmoothSwipexit.h
@@ -0,0 +1,19 @@
+//
+//  SmoothSwipexit.h
+//  SmoothSwipexit
+//
+//  Created by Johan Helmertz on 2019-03-08.
+//  Copyright © 2019 Johan Helmertz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmoothSwipexit.
+FOUNDATION_EXPORT double SmoothSwipexitVersionNumber;
+
+//! Project version string for SmoothSwipexit.
+FOUNDATION_EXPORT const unsigned char SmoothSwipexitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmoothSwipexit/PublicHeader.h>
+
+
